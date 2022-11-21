dfabcb8920b598cf3dc58802debf8c4ba02cb97d
KobyS82 <kobysillito@gmail.com>
2022-11-20 19:47:14 -0600

    quiz now only shows when start is clicked

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,15 @@
 var questionPrompt = document.querySelector('#question');
-var startPrompt = document.querySelector('.game');
+var displayGame = document.querySelector('.game');
 var start = document.querySelector('#start-button');
 var choicesEl = document.querySelector('#choices');
 var initials = document.querySelector('#initials');
 var formBtn = document.querySelector('#formBtn');
+var timerEl = document.querySelector('#timer');
 
 var questionIndex = 0;
 var startTime = 50;
 var score = 0;
+var turn = false;
 
 var questions = [
     {question: "what is my favorite color?",
@@ -22,17 +24,25 @@ var questions = [
 
 ]
 
+function displayGameOver(TF){
+
+}
+
+
 function startGame(){
+    displayGame.setAttribute('style','display: visible')
     timer();
     getQuestion();
 }
 
 function timer(){
+
     var interval = setInterval(function(){
         if (startTime < 0){
             clearInterval(interval);
             gameOver();
         } else {
+            timerEl.setAttribute('value', startTime);
             console.log(startTime);
             startTime--;
         }
@@ -48,10 +58,10 @@ function getQuestion(){
     choicesEl.innerHTML = '';
 
 
+
     for (var i = 0; i < currentQuestion.choices.length; i++) {
         var option = currentQuestion.choices[i];
         var choiceBtn = document.createElement('button');
-        choiceBtn.setAttribute('style', 'display: visible');
         choiceBtn.setAttribute('value', option);
         choiceBtn.textContent = option;
         choicesEl.appendChild(choiceBtn);
@@ -77,7 +87,7 @@ function getQuestion(){
     }
 }
 
-
+//function tha\
 function gameOver(){
     //TODO change visuals
     console.log('game over');
@@ -85,9 +95,12 @@ function gameOver(){
 
 }
 
+// High score board
 function highScore(){
     var init = initials.value;
-    localStorage.setItem(init, score);
+    var timeScore = timer.interval;
+    //localStorage.setItem(init, score);
+    localStorage.setItem(init, timeScore);
 }
 
 start.addEventListener("click", startGame);
