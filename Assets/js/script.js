ae5e388aa8739fc6d5856da1f617dc3bf18708dc
KobyS82 <kobysillito@gmail.com>
2022-11-20 21:03:38 -0600

    added scores and scoreboard(kinda)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,47 +1,69 @@
 var questionPrompt = document.querySelector('#question');
 var displayGame = document.querySelector('.game');
 var start = document.querySelector('#start-button');
+var headEl = document.querySelector('#startHead');
 var choicesEl = document.querySelector('#choices');
 var initials = document.querySelector('#initials');
 var formBtn = document.querySelector('#formBtn');
 var timerEl = document.querySelector('#timer');
+var gameOverEl = document.querySelector('#gameOver');
+var answerEl = document.querySelector('#answer');
+var scoresEl = document.querySelector('.scores');
+var scoresList = document.querySelector('#scoresList');
 
 var questionIndex = 0;
 var startTime = 50;
 var score = 0;
 var turn = false;
+var answerTrue = 'Correct!'
+var answerFalse = 'Incorrect :(';
 
+// Questions for the Quiz
 var questions = [
-    {question: "what is my favorite color?",
+    {question: "What is my favorite color?",
      answer: "purple",
-     choices: ["blue", "black", "orange", "purple"]
+     choices: ["blue", "black", "brown", "purple"]
     },
-    {question: "test?",
+    {question: "Color of the sky during the day?",
      answer: "blue",
-     choices: ["blue", "black", "orange", "purple"]
+     choices: ["blue", "black", "brown", "purple"]
     },
-
-
+    {question: "Color of a football",
+     answer: "brown",
+     choices: ["blue", "black", "brown", "purple"]
+    },
+    {question: "Color of eggplant",
+     answer: "purple",
+     choices: ["blue", "black", "brown", "purple"]
+    },
+    {question: "Color of the Sea",
+     answer: "blue",
+     choices: ["blue", "black", "brown", "purple"]
+    },
+    {question: "Color of dirt",
+     answer: "brown",
+     choices: ["blue", "black", "brown", "purple"]
+    }
 ]
 
-function displayGameOver(TF){
-
-}
-
-
+// Starts the game
 function startGame(){
-    displayGame.setAttribute('style','display: visible')
+    displayGame.setAttribute('style','display: visible');
+    start.setAttribute('style', 'display: none');
+    headEl.textContent = '';
     timer();
     getQuestion();
 }
 
-function timer(){
 
+// Creates and tracks the timer
+function timer(){
     var interval = setInterval(function(){
         if (startTime < 0){
             clearInterval(interval);
             gameOver();
         } else {
+            
             timerEl.setAttribute('value', startTime);
             console.log(startTime);
             startTime--;
@@ -52,12 +74,40 @@ function timer(){
 }
 
 
+// Displays game over
+function gameOver(){
+    gameOverEl.setAttribute('style', 'display: initial');
+    displayGame.setAttribute('style', 'display: none');
+    headEl.textContent = 'Game Over!';
+}
+
+// Tracks high scores
+function setHighScore(){
+    var init = initials.value;
+    //var person = {init, score};
+    //localStorage.setItem('user', JSON.stringify(person));
+    localStorage.setItem(init, score);
+    //var timeScore = timer.interval;
+    //localStorage.setItem(init, timeScore);
+    highScoresPage();
+}
+
+// Leader Board
+function highScoresPage(){
+    //var user = JSON.parse(localStorage.getItem('user'));
+    var userScore = localStorage.getItem(initials.value);
+    gameOverEl.setAttribute('style', 'display: none');
+    headEl.textContent = 'Your Score!';
+    console.log(initials.value);
+    scoresList.textContent = initials.value + ": " + userScore;
+
+}
+
+// Gets questions for quiz and checks if the user clicks the right one
 function getQuestion(){
     var currentQuestion = questions[questionIndex];
     questionPrompt.textContent = currentQuestion.question;
     choicesEl.innerHTML = '';
-
-
 
     for (var i = 0; i < currentQuestion.choices.length; i++) {
         var option = currentQuestion.choices[i];
@@ -68,8 +118,10 @@ function getQuestion(){
         choiceBtn.addEventListener('click', function(event){
 
             if (event.target.textContent === questions[questionIndex].answer){
-                //TODO change to display
-                console.log('correct');
+                answerEl.textContent = answerTrue;
+                setTimeout(function(){
+                    answerEl.textContent = '';
+                }, 3000);
                 questionIndex++;
                 score++;
                     if (questionIndex < questions.length) {
@@ -78,30 +130,17 @@ function getQuestion(){
                         gameOver();
                     }
             }  else {
-                console.log('incorrect');
+                answerEl.textContent = answerFalse;
+                setTimeout(function(){
+                    answerEl.textContent = '';
+                }, 3000);
                 startTime -= 10;
 
             }
-        }
-    )
+        })
     }
 }
 
-//function tha\
-function gameOver(){
-    //TODO change visuals
-    console.log('game over');
-
-
-}
-
-// High score board
-function highScore(){
-    var init = initials.value;
-    var timeScore = timer.interval;
-    //localStorage.setItem(init, score);
-    localStorage.setItem(init, timeScore);
-}
-
+// Listens for clicks on buttons
 start.addEventListener("click", startGame);
-formBtn.addEventListener("click", highScore);
+formBtn.addEventListener("click", setHighScore);
